08dd736bcb2da21069cea60ab4147ab533edc7f7
Dedi Dwi Saputra <DEDID1@intra.excelcom.co.id>
2019-04-26 16:23:27 +0700

    add: initial commit CICD

---
+++ src/test/java/com/dsaputra/jenkins/cicdjenkinstest/CicdJenkinsTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dsaputra.jenkins.cicdjenkinstest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdJenkinsTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
